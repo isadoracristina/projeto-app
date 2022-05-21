a205a1b10e2f0d309dd9ec0dbaf154c0f1882677
AndreOGodoy <godoy.andre@hotmail.com>
2022-05-21 17:33:17 -0300

    Backend: Estrutura Inicial + Entidades
    Definição inicial da estrutura de arquivos, além das entidades
    basicas 'User', 'Recipe' e 'Ingredient'

---
+++ backend/src/domain/entities/recipe.py
@@ -0,0 +1,10 @@
+from typing import List
+from pydantic.main import BaseModel
+
+from ingredient import Ingredient
+
+class Recipe(BaseModel):
+    name: str
+    ingredients: List[Ingredient]
+    preparation_time_sec: int
+    preparation_method: str
