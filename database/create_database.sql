06c9c9944d4db6764d0b9b8fb87f31c96f6ea172
Julia Fonseca <42375312+jufs128@users.noreply.github.com>
2022-05-29 00:51:55 -0300

    Mudando para script de SQLite

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,20 +1,16 @@
-CREATE DATABASE despensa;
-USE despensa;
-
 CREATE TABLE Users
 (
-    id_user INT NOT NULL,
+    id_user INT PRIMARY KEY NOT NULL,
     email VARCHAR(50) NOT NULL,
     psswd VARCHAR(100) NOT NULL,
     hash_psswd VARCHAR(100) NOT NULL,
     name_user VARCHAR(50) NOT NULL,
-    date_registered DATETIME NOT NULL,
-    PRIMARY KEY (id_user)
+    date_registered DATETIME NOT NULL
 );
 
 CREATE TABLE Recipes
 (
-    id_recipe INT NOT NULL,
+    id_recipe INT PRIMARY KEY NOT NULL,
     id_user INT NOT NULL,
     name_recipe VARCHAR(50) NOT NULL,
     img_addr VARCHAR(50),
@@ -24,38 +20,34 @@ CREATE TABLE Recipes
     observation TEXT,
     last_made DATETIME,
     date_registered DATETIME NOT NULL,
-    PRIMARY KEY (id_recipe),
+    pantry_onlu BOOL NOT NULL,
     FOREIGN KEY (id_user) REFERENCES Users(id_user)
 );
 
 CREATE TABLE Ingredients
 (
-	id_ingredient INT NOT NULL,
-    name_ingredient VARCHAR(20) NOT NULL,
-    PRIMARY KEY(id_ingredient)
+	id_ingredient INT PRIMARY KEY NOT NULL,
+    	name_ingredient VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Tags
 (
-	id_tag INT NOT NULL,
-    description_tag VARCHAR(20) NOT NULL,
-    PRIMARY KEY(id_tag)
+	id_tag INT PRIMARY KEY NOT NULL,
+    	description_tag VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Recipe_Ingredients
 (
-	id_recipe INT NOT NULL,
-    id_ingredient INT NOT NULL,
-    PRIMARY KEY(id_recipe, id_ingredient),
-    FOREIGN KEY (id_recipe) REFERENCES Recipes(id_recipe),
-    FOREIGN KEY (id_ingredient) REFERENCES Ingredients(id_ingredient)
+	id_recipe INT PRIMARY KEY NOT NULL,
+    	id_ingredient INT NOT NULL,
+    	FOREIGN KEY (id_recipe) REFERENCES Recipes(id_recipe),
+    	FOREIGN KEY (id_ingredient) REFERENCES Ingredients(id_ingredient)
 );
 
 CREATE TABLE Recipe_Tags
 (
-	id_recipe INT NOT NULL,
-    id_tag INT NOT NULL,
-    PRIMARY KEY(id_recipe,id_tag),
-    FOREIGN KEY (id_recipe) REFERENCES Recipes(id_recipe),
-    FOREIGN KEY (id_tag) REFERENCES Tags(id_tag)
+	id_recipe INT PRIMARY KEY NOT NULL,
+    	id_tag INT NOT NULL,
+    	FOREIGN KEY (id_recipe) REFERENCES Recipes(id_recipe),
+    	FOREIGN KEY (id_tag) REFERENCES Tags(id_tag)
 );
