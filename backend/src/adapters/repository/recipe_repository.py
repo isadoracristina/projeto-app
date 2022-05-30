7a80e26df4fd5a2232b065959c4b6030c770e9d6
Julia Fonseca <42375312+jufs128@users.noreply.github.com>
2022-05-30 15:58:24 -0300

    Algumas funções do db

--- backend/src/adapters/repository/recipe_repository.py
+++ backend/src/adapters/repository/recipe_repository.py
@@ -1,27 +1,41 @@
-from pydantic import BaseModel
+from unicodedata import name
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import func
+import datetime
+from sqlalchemy import insert, delete
 
 from backend.src.domain.ports.recipe_repository import RecipeRepository
 
 from backend.src.interface.database.fake_db import RECIPES as FakeDB_Recipes
 
 from backend.src.domain.entities.recipe import Recipe
+from backend.src.domain.entities.user import User
 from backend.src.domain.entities.ingredient import Ingredient
 
+from backend.src.interface.database.database import SessionLocal
+import backend.src.interface.database.models as models
+
 class RecipeRepositoryImpl(RecipeRepository):
-    async def get(self, id: int) -> Recipe:
-        if id in FakeDB_Recipes:
-            recipe_dict = FakeDB_Recipes[id]
-            ingredients = [Ingredient(name=x) for x in recipe_dict["ingredients"]]
-            recipe_dict["ingredients"] = ingredients
-            return Recipe(**recipe_dict, id=id)
+    async def get(self, db: Session, id: int) -> Recipe:
+        return db.query(models.Recipe).filter(models.Recipe.id_recipe == id).first()
 
-    async def create(self, recipe: Recipe) -> Recipe:
-        id = len(FakeDB_Recipes)
-        FakeDB_Recipes[id] = vars(recipe)
-        FakeDB_Recipes[id].update({"id": id})
-        return FakeDB_Recipes[id]
+    async def create(self, db: Session, recipe: Recipe, user: User) -> Recipe:
+        new_id = SessionLocal().query(func.max(models.Recipe.id_recipe)).scalar()
+        if new_id == None:
+            new_id = 1
+        else:
+            new_id += 1
+        db_item = models.Recipe(id_recipe=new_id, id_user=user.id_user, name_recipe=recipe.name,
+                                img_addr=recipe.img_addr, prep_time=recipe.preparation_time_sec,
+                                prep_method=recipe.preparation_method, rating=recipe.rating,
+                                observation=recipe.observation, last_made=None,
+                                date_registered=datetime.datetime.now(), pantry_only=recipe.pantry_only)
+        db.add(db_item)
+        db.commit()
+        db.refresh(db_item)
+        return db_item
 
-    async def update(self, recipe: Recipe) -> Recipe:
+    async def update(self, db: Session, recipe: Recipe) -> Recipe:
         if recipe.id not in FakeDB_Recipes:
             return NotImplementedError
 
--- backend/src/adapters/repository/user_repository.py
+++ backend/src/adapters/repository/user_repository.py
@@ -1,25 +1,32 @@
 from pydantic import BaseModel
+import datetime
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import func
 
-from backend.src.domain.entities.user import User
 from backend.src.domain.ports.user_repository import UserRepository
 
-from backend.src.interface.database.fake_db import USERS as FakeDB_Users
-from backend.src.interface.database.user_model import UserModel
+from backend.src.interface.database.database import SessionLocal
+import backend.src.interface.database.models as models
 
 class UserRepositoryImpl(UserRepository):
-    async def get(self, id: int) -> User:
-        raise NotImplementedError
+    async def get(self, db: Session, id: int):
+        return db.query(models.User).filter(models.User.id_user == id).first()
 
-    async def get_by_name(self, name: str) -> User:
-        if name in FakeDB_Users:
-            user_dict = FakeDB_Users[name]
-            return UserModel(**user_dict)
+    async def get_by_name(self, db: Session, name: str):
+        return db.query(models.User).filter(models.User.name_user == name).first()
 
-    async def create(self, username: str, hashed_password: str):
-        if username in FakeDB_Users:
-            # Deve retornar erro: usuário ja existente
-            raise NotImplementedError
+    async def get_by_email(self, db: Session, email: str):
+        return db.query(models.User).filter(models.User.email == email).first()
 
-        FakeDB_Users[username] = { "id": len(FakeDB_Users), "name": username, "hashed_password": hashed_password }
-        print(FakeDB_Users)
+    def create(self, db: Session, username: str, email: str, hashed_password: str):
+        new_id = SessionLocal().query(func.max(models.User.id_user)).scalar()
+        if new_id == None:
+            new_id = 1
+        else:
+            new_id += 1
+        db_user = models.User(id_user= new_id,email=email, hash_psswd=hashed_password, name_user=username, date_registered=datetime.datetime.now()) 
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
 
