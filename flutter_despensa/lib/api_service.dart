15939ab89da059928e8579ec0efc834693354e04
AndreOGodoy <godoy.andre@hotmail.com>
2022-05-30 19:16:14 -0300

    Frontend: Criação de classe para comunicação com Back-End

---
+++ flutter_despensa/lib/api_service.dart
@@ -0,0 +1,65 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ApiServices {
+  updateToken(String token) async {
+    var preferences = await SharedPreferences.getInstance();
+    preferences.setString('token', token);
+  }
+
+  login(String username, String password) async {
+    var preferences = await SharedPreferences.getInstance();
+    var api_root = await preferences.getString('api_root') as String;
+
+    var body_str =
+        'grant_type=password&username=${username}&password=${password}';
+
+    var response = await http.post(
+      Uri.parse(api_root + '/token'),
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: body_str,
+    );
+
+    // Tratar caso resposta não seja OK
+
+    var body_json = json.decode(response.body);
+    var token = body_json['token_type'] + ' ' + body_json['access_token'];
+    preferences.setString('token', token);
+    preferences.setString('is_logged_in', true);
+  }
+
+  register(String username, String email, String password) async {
+    var preferences = await SharedPreferences.getInstance();
+    var api_root = await preferences.getString('api_root') as String;
+
+    var response = await http.post(
+      Uri.parse(api_root + '/register'),
+      headers: <String, String>{'Content-Type': 'application/json'},
+      body: jsonEncode(<String, String>{
+        'username': username,
+        'email': email,
+        'password': password
+      }),
+    );
+
+    // Tratar caso resposta não seja OK
+  }
+
+  getRecipeById(int id) async {
+    var preferences = await SharedPreferences.getInstance();
+    var api_root = await preferences.getString('api_root') as String;
+
+    var response = await http.get(
+        Uri.parse(api_root + '/recipe/' + id.toString()),
+        headers: <String, String>{
+          'Authorization': await preferences.getString('token') as String,
+        });
+
+    // Tratar caso resposta não seja OK
+    return json.decode(response.body);
+  }
+}
