6aa7cf3d3e46514abe94828619f2491830fe003f
Julia Fonseca <42375312+jufs128@users.noreply.github.com>
2022-06-01 09:06:07 -0300

    Retornando nome de ingredientes e tags de receitas

--- backend/src/interface/api/main.py
+++ backend/src/interface/api/main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 from fastapi import FastAPI, Depends, HTTPException, Request, status, Path
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
@@ -17,6 +17,8 @@ from backend.src.adapters.repository.tag_repository import TagRepositoryImpl
 
 from backend.src.domain.entities.user import User
 from backend.src.domain.entities.recipe import Recipe
+from backend.src.domain.entities.recipe_ingredient import Recipe_Ingredient
+from backend.src.domain.entities.recipe_tag import Recipe_Tag
 from backend.src.domain.entities.ingredient import Ingredient
 from backend.src.domain.entities.tag import Tag
 
@@ -60,6 +62,22 @@ class UserRegister(BaseModel):
     username: str
     email: str
     password: str
+
+class RecipeGet(BaseModel):
+    id: int
+    id_user: int
+    name: str
+    img_addr: Union[str, None]
+    preparation_time_sec: int
+    preparation_method: str
+    rating: float
+    observation: str
+    last_made: Union[datetime, None]
+    pantry_only: bool
+    ingredients: List[Recipe_Ingredient]
+    tags: List[Recipe_Tag]
+    ingredients_names: list
+    tags_names: list
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -162,13 +180,24 @@ async def get_recipe(
         db: Session = Depends(get_db)
 ):
     recipe_model = await RecipeRepository.get(db, recipe_id)
-    recipe_model.prep_time = 10
-    recipe = Recipe(id=recipe_model.id_recipe, id_user=recipe_model.id_user,
+    
+    list_ing_names = []
+    list_tag_names = []
+    for r in recipe_model.ingredients:
+        i = await IngredientRepository.get(db, r.id_ingredient)
+        print(i.id_ingredient, i.name_ingredient)
+        list_ing_names.append(i)
+    for r in recipe_model.tags:
+        t = await TagRepository.get(db, r.id_tag)
+        list_tag_names.append(t)
+
+    recipe = RecipeGet(id=recipe_model.id_recipe, id_user=recipe_model.id_user,
                     name=recipe_model.name_recipe, img_addr=recipe_model.img_addr,
                     preparation_time_sec=recipe_model.prep_time, preparation_method= recipe_model.prep_method,
                     rating=recipe_model.rating, observation=recipe_model.observation,
                     last_made=recipe_model.last_made, pantry_only=recipe_model.pantry_only,
-                    ingredients=recipe_model.ingredients, tags=recipe_model.tags)
+                    ingredients=recipe_model.ingredients, tags=recipe_model.tags,
+                    ingredients_names= list_ing_names, tags_names=list_tag_names)
 
     return recipe
 
@@ -193,12 +222,22 @@ async def get_all_recipes(
 
     list_recipes = []
     for recipe_model in await RecipeRepository.get_all(db, user_id=current_user.id_user):
-        recipe = Recipe(id=recipe_model.id_recipe, id_user=recipe_model.id_user,
+        list_ing_names = []
+        list_tag_names = []
+        for r in recipe_model.ingredients:
+            i = await IngredientRepository.get(db, r.id_ingredient)
+            list_ing_names.append(i)
+        for r in recipe_model.tags:
+            t = await TagRepository.get(db, r.id_tag)
+            list_tag_names.append(t)
+
+        recipe = RecipeGet(id=recipe_model.id_recipe, id_user=recipe_model.id_user,
                     name=recipe_model.name_recipe, img_addr=recipe_model.img_addr,
                     preparation_time_sec=recipe_model.prep_time, preparation_method= recipe_model.prep_method,
                     rating=recipe_model.rating, observation=recipe_model.observation,
                     last_made=recipe_model.last_made, pantry_only=recipe_model.pantry_only,
-                    ingredients=recipe_model.ingredients, tags=recipe_model.tags)
+                    ingredients=recipe_model.ingredients, tags=recipe_model.tags,
+                    ingredients_names= list_ing_names, tags_names=list_tag_names)
         list_recipes.append(recipe)
 
     return list_recipes
