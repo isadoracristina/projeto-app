f10dd6a993463b856279bdad4c04f299ffdb77f0
isadoracristina <isadorac2008@hotmail.com>
2022-05-31 22:13:28 -0300

    Criação da função de criar tag

--- backend/src/adapters/repository/tag_repository.py
+++ backend/src/adapters/repository/tag_repository.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import func
 
 from backend.src.domain.ports.tag_repository import TagRepository
+
+from backend.src.domain.entities.tag import Tag
 
 from backend.src.interface.database.database import SessionLocal
 import backend.src.interface.database.models as models
@@ -13,4 +16,18 @@ class TagRepositoryImpl(TagRepository):
         return db.query(models.Tag).filter(models.Tag.description_tag == name).first()
 
     async def get_all(self, db: Session):
-        return db.query(models.Ingredient).all()
+        return db.query(models.Tag).all()
+
+    async def create(self, db: Session, tag: Tag) -> Tag:
+        new_id = SessionLocal().query(func.max(models.Tag.id_tag)).scalar()
+        if new_id == None:
+            new_id = 1
+        else:
+            new_id += 1
+        db_item = models.Tag(id_tag=new_id, description_tag=tag.description)
+
+        db.add(db_item)
+        db.commit()
+        db.refresh(db_item)
+
+        return db_item
